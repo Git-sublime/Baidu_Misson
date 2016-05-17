fe58e2148d4f1b871717c284f2bda1d2fcbdfe78
GIT-sublime <249262363@qq.com>
2016-05-17 20:25:18 +0800

    misson

---
+++ Baidu_Mission_7.js
@@ -0,0 +1,21 @@
+// window.onload=function(){
+// 	document.getElementById("btn_01").onclick=function(){
+// 		document.getElementById("R_box_01").style.top="0px";
+// 	}
+// 	document.getElementById("btn_02").onclick=function(){
+// 		document.getElementById("R_box_02").style.top="0px";
+// 	}
+// 	document.getElementById("btn_03").onclick=function(){
+// 		document.getElementById("R_box_03").style.top="0px";
+// 	}
+// }
+	
+	function show1(){
+		document.getElementById("R_box_01").style.top="0px";
+	}
+	function show2(){
+		document.getElementById("R_box_02").style.top="0px";
+	}
+	function show3(){
+		document.getElementById("R_box_03").style.top="0px";
+	}
